b1e785abe326855073741fe929b2ffc2eaf16eab
Naveen <kumarnav909@gmail.com>
2023-12-04 15:52:18 +0530

    lab4 done

---
+++ App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+// import Button from './Button'; // Import the Button component
+import Toolbar from './Toolbar2';
+function App() {
+  return (
+    <div className="App">
+      {/* <h1>React Events Lab</h1> */}
+      <h1>Reading Props in Event Handlers</h1>
+      <Toolbar />
+    </div>
+  );
+}
+export default App;
